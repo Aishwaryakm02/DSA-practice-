ef826bfe395abd4b88b1498bbe1813f9157163c0
Aishwarya K M <aishwaryakm4842@gmail.com>
2026-04-15 22:30:11 +0530

    Add function to calculate sum of numbers

--- Nnumber.cpp
+++ Nnumber.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+using namespace std;
 
+int totsum(int n){
+    int sum=0,i;
+    for(i=1;i<=n;i++){
+        sum=sum+i;
+    }
+    return(sum);
+    
+    
+    
+}
+int main(){
+    cout<<"sum of numbers:---"<<totsum(5);
+    return 0;
+}
