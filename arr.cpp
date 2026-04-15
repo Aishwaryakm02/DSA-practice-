bd37182868831e3731b1ae7e61b2636a93a5bcc8
Aishwarya K M <aishwaryakm4842@gmail.com>
2026-04-15 23:12:33 +0530

    Implement display and input for marks array
    Added a program to display and take input for marks.

--- arr.cpp
+++ arr.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int marks[]={10,20,60,70,44};
+    int size=5;
+    for(int i=0;i<size;i++){
+        cout<<marks[i]<<endl;
+    }
+
+
+    return 0;
+}
+
+
+
+//taking input
+
+
+#include <iostream>
+using namespace std;
+
+int main(){
+  int marks[5];
+  for(int i=0;i<5;i++){
+      cin>>marks[i];
+  }
+}
 
