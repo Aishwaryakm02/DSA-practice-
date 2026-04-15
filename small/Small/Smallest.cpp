d3020d57968ddebb846382effbdaf2c293dd6c88
Aishwarya K M <aishwaryakm4842@gmail.com>
2026-04-15 23:17:56 +0530

    Find the smallest number in an array
    Fix loop increment and output formatting.

--- small/Small/Smallest.cpp
+++ small/Small/Smallest.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <climits>
+using namespace std;
 
+int main() {
+    int nums[]={10,4,70,43,89};
+    int smallest=INT_MAX;
+    for(int i=0;i<5;i++){
+        if(nums[i]<smallest){
+            smallest=nums[i];
+        }
+    }
+    cout<<"smallest=---"<<smallest;
+    
+
+    return 0;
+}
