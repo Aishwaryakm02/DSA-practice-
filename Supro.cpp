8c0b6b09c6beaeea46ee719b136108d42931f0be
Aishwarya K M <aishwaryakm4842@gmail.com>
2026-04-16 23:58:15 +0530

    Correct loop increment in op function
    Fixes the loop increment in the op function.

--- Supro.cpp
+++ Supro.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+using namespace std;
 
+void op(int arr[],int size){
+    int sum=0,pro=1;
+    for(int i=0;i<size;i++){
+        sum=sum+arr[i];
+        pro=pro*arr[i];
+        
+    }
+    cout<<"sum of all ele"<<sum<<endl;
+    cout<<"product"<<pro<<endl;
+    
+    
+}
+
+int main() {
+    int arr[]={1,2};
+    int size=2;
+    op(arr,size);
+    
+    
+    
+
+    return 0;
+}
