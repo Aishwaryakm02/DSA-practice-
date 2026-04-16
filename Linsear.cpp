4712d5c9b87809ead59f26702230c05f69b50210
Aishwarya K M <aishwaryakm4842@gmail.com>
2026-04-16 23:18:00 +0530

    Correct loop condition in ls function
    Fix the loop condition in the ls function to prevent out-of-bounds access.

--- Linsear.cpp
+++ Linsear.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+using namespace std;
 
+int ls(int arr[],int size,int tar){
+    for(int i=0;i<=size;i++){
+        if(arr[i]==tar){
+            return i;
+        }
+        
+    }
+    return -1;
+}
+
+int main() {
+    int arr[]={1,2,3,5,8};
+    int size=5;
+    int target=3;
+    cout<<ls(arr,size,target)<<endl;
+
+    return 0;
+}
