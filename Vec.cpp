a3021ea94bda8f518eb756c8d286df321a2f287d
Aishwarya K M <aishwaryakm4842@gmail.com>
2026-04-22 23:30:56 +0530

    Initialize vector and display elements
    Added a simple program to initialize and display vector elements.

--- Vec.cpp
+++ Vec.cpp
@@ -1,1 +1,13 @@
+//initialisation of vector
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<int> v={1,2,3};
+    cout<<v[0]<<endl; //getting the elements 
+    cout<<v[2]<<endl;
+
+    return 0;
+}
 
