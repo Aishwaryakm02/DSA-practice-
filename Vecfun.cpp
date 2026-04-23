f998149349ec12789320159e560b768609fee4a2
Aishwarya K M <aishwaryakm4842@gmail.com>
2026-04-24 00:04:41 +0530

    Modify main function to enhance vector operations
    Updated the vector initialization and added functionality to display elements and size.

--- Vecfun.cpp
+++ Vecfun.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
 #include <vector>
+using namespace std;
 
-int main(){
-  vector<int> vec={1,3,7,5};
-  //size
-  cout<<"size od vector:-"<<size(vec);
-  return 0;
+int main() {
+    vector<int> vec={2,6,4,8};
+    //to determine no. of elements 
+    cout<<"size of vector"<<vec.size();
+    //adds element at the end
+    vec.push_back(25);
+    vec.push_back(45);
+  // to print every element in vecto
+    for(int i:vec){
+        cout<<i<<endl;
+    }
+
+    
+
+    return 0;
 }
 
+output:--size of vector4
+         2
+        6
+        4
+        8
+        25
+       45
