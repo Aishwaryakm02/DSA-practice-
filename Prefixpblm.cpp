effd55dee27785e5b3e9d7e173869629e5545749
Aishwarya K M <aishwaryakm4842@gmail.com>
2026-04-30 22:07:26 +0530

    Add prefix finding logic in Prefixpblm.cpp
    Implement prefix finding for an array of strings.

--- Prefixpblm.cpp
+++ Prefixpblm.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    string c[3]={"flow","flowers","fling"};
+    string result ="";
+    for(int i=0;i<c[0].length();i++){
+        int ch=c[0][i];
+        for(int j=0;j<3;j++){
+            if(j>=c[i].length() || c[j][i]!=ch){
+                cout<<result;
+                return 0;
+            }
+    
+            
+                
+            
+            
+        }
+        result+=ch;
+    }
+
+    cout<<result;
+
+
+    return 0;
+}
