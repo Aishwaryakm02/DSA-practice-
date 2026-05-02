939ce3f0595c7623722fd21ae38a222751d3442a
Aishwarya K M <aishwaryakm4842@gmail.com>
2026-05-03 00:09:15 +0530

    Add brute force solution for maximum subarray sum
    Implement brute force algorithm to find maximum subarray sum.

--- Brute.cpp
+++ Brute.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int arr[] = {1, -2, 3, 4, -1};
+    int n = 5;
+
+    int maxSum = INT_MIN;
+
+    for(int st = 0; st < n; st++) {
+        for(int end = st; end < n; end++) {
+            
+            int sum = 0;
+
+            for(int i = st; i <= end; i++) {
+                sum += arr[i];
+            }
+
+            if(sum > maxSum) {
+                maxSum = sum;
+            }
+        }
+    }
+
+    cout << "Maximum Subarray Sum: " << maxSum;
+
+    return 0;
+}
