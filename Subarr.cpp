b0d88047da6b6e1dcd5531d28f53597d06056069
Aishwarya K M <aishwaryakm4842@gmail.com>
2026-05-03 00:07:08 +0530

    Implement subarray printing in Subarr.cpp
    Print all subarrays of the given array.

--- Subarr.cpp
+++ Subarr.cpp
@@ -1,1 +1,21 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() {
+// Write C++ code here
+int arr[4]={1,2,3,4};
+for(int st=0;st<4;st++){
+  for(int end=st;end<4;end++){
+      for(int i=st;i<=end;i++){
+          cout<<arr[i];
+      }
+      cout<<" ";
+  }
+  cout<<endl;
+}
+
+return 0;
+
+}
 
