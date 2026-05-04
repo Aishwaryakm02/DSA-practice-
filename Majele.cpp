df79e330efbb1870d1cf301c6d47ce518783a496
Aishwarya K M <aishwaryakm4842@gmail.com>
2026-05-04 22:13:39 +0530

    Add majority element function in Majele.cpp
    Implement a function to find the majority element in a vector.

--- Majele.cpp
+++ Majele.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
 
+int maj(vector <int> v){
+    for(int val:v){
+        int count=0;
+        for(int ele:v){
+            if(ele==val){
+                count++;
+                
+            }
+        }
+        if(count>2){
+            return val;
+        }
+    }
+    return -1;
+}
+
+int main() {
+    vector <int> v={1,1,2,2,1};
+    int value=maj(v);
+    cout<<value;
+
+    return 0;
+}
