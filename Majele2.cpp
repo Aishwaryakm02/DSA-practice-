4c898664ea45c4133008ea225ef8a978e6146eb6
Aishwarya K M <aishwaryakm4842@gmail.com>
2026-05-05 21:38:17 +0530

    Add frequency check for elements in vector
    Implement logic to find and print the first element with a frequency greater than 2 in a vector.

--- Majele2.cpp
+++ Majele2.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
 
+int main() {
+    vector <int> v={1,1,2,2,2};
+    int freq=1,ans=v[0];
+    for(int i=1;i<v.size();i++){
+        if(v[i]==v[i-1]){
+            freq++;
+        }
+        else{
+            freq=1;ans=v[i];
+        }
+        if(freq>2){
+            cout<<ans;
+            break;
+        }
+    }
+    
+
+    return 0;
+}
