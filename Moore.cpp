3f819720e217b7cdc67d323b1c7a94317638eeca
Aishwarya K M <aishwaryakm4842@gmail.com>
2026-05-05 21:40:26 +0530

    Add Moore's Voting Algorithm implementation
    Implement Moore's Voting Algorithm to find the majority element in a vector.

--- Moore.cpp
+++ Moore.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
 
+int main() {
+    vector <int> v={1,2,2,2,1,2};
+    int freq=0,ans=0;
+    for(int i=0;i<v.size();i++){
+        if(freq==0){
+            ans=v[i];
+        }
+        if(v[i]==ans){
+            freq++;
+        }else{
+            freq--;
+        }
+    }
+    cout<<ans;
+    
+    
+
+    return 0;
+}
